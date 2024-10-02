8b36911a438ce9c9688bedb2fd8c67da9af1f529
brooke-woodland <brooke@woodlandfamily.org>
2024-10-02 12:09:18 -0600

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFun2
+{
+    internal class PrintStuff
+    {
+        private string language;
+
+        //constructor: automatically called when program runs
+      public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+
+      public PrintStuff()
+        {
+            language = "EN";
+        }
+        public void PrintName(string firstName)
+        {   
+            if (language == "EN")
+            {
+            System.Console.WriteLine("hello, " + firstName + "!");
+     
+            }
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("oi, " + firstName + "!");
+            }
+        }
+
+        public void PrintName()
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("hello, winner!");
+
+            }
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("oi, winner!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CSharpFun2;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff();
+
+        string name = "";
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName();
+    }
+}
